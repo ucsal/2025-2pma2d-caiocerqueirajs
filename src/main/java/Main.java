ac92bf517ebbed45c03d0ce318834a843bac7987
caiocerqueirajs <caiocerqueira.silva@ucsal.edu.br>
2025-09-02 23:26:34 -0300

    Alterei Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,20 @@
 import br.com.mariojp.solid.dip.*;
 
 public class Main {
-	public static void main(String[] args) {
-		System.setProperty("DRY_RUN", "true"); // desejável não chamar SMTP
-		var notifier = new EmailNotifier();
-		// Estado inicial: vai lançar IllegalStateException (SMTP indisponível)
-		notifier.welcome(new User("Ana", "ana@example.com"));
-		System.out.println("Email enviado!");
-	}
+    public static void main(String[] args) {
+        boolean dryRun = Boolean.parseBoolean(System.getProperty("DRY_RUN", "false"));
+
+        MailSender sender;
+        if (dryRun) {
+            sender = new NoopMailSender();
+        } else {
+            SmtpClient smtpClient = new SmtpClient();
+            sender = new SmtpMailSender(smtpClient);
+        }
+
+        EmailNotifier notifier = new EmailNotifier(sender);
+        notifier.welcome(new User("Caio", "caio@example.com"));
+
+        System.out.println("Email enviado!");
+    }
 }
